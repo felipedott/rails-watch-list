7a636675b00331fcb5b7575dc7149979333319ce
Felipe Dottling <felipedott@gmail.com>
2022-11-18 13:55:57 -0500

    review buggado pls fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
