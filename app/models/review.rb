82984d1c35197ce9da4c6b94d1f168d4cdb90be8
Felipe Dottling <felipedott@gmail.com>
2022-11-18 14:32:22 -0500

    pronto capim

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
 
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, in: 0..5 }
+  validates :comment, presence: true
 end
